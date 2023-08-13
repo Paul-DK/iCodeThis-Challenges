51b37276269da943635ca3774272e3574a1409ba
Paul <pdbhomemail@gmail.com>
2023-08-13 03:05:25 +0200

    Javascript, Modal, Social Icons

---
+++ Challenge_283-Book_Card/js/main.js
@@ -0,0 +1,64 @@
+// Read More Button
+
+document.querySelector(".morebutton").addEventListener("click", function() {
+  var readMoreDiv = document.querySelector(".read-more");
+  var snippet = document.querySelector(".read-more .snippet");
+  
+  if (readMoreDiv.style.height === "0px" || readMoreDiv.style.height === "") {
+    readMoreDiv.style.height = snippet.offsetHeight + "px";
+  } else {
+    readMoreDiv.style.height = "0px";
+  }
+});
+
+// Share Button
+
+var modal = document.getElementById("shareModal");
+var btn = document.querySelector(".share");
+var span = document.querySelector(".close-modal");
+
+btn.onclick = function() {
+  modal.style.display = "block";
+  setTimeout(function() { modal.classList.add('show'); }, 50); // Add the class after a short delay
+}
+
+span.onclick = function() {
+  modal.classList.remove('show');
+  setTimeout(function() { modal.style.display = "none"; }, 500); // Remove the display after the transition
+}
+
+window.onclick = function(event) {
+  if (event.target === modal) {
+    modal.classList.remove('show');
+    setTimeout(function() { modal.style.display = "none"; }, 500); // Remove the display after the transition
+  }
+}
+
+// Close Button
+
+const closeButton = document.querySelector('.close');
+const card = document.querySelector('.card');
+const body = document.querySelector('body');
+
+const fadeOut = () => {
+  card.classList.add('fade-out');
+  body.classList.add('fade-out');
+};
+
+const fadeIn = () => {
+  card.classList.remove('fade-out');
+  body.classList.remove('fade-out');
+};
+
+closeButton.addEventListener('click', () => {
+  fadeOut();
+  
+  setTimeout(() => {
+    card.style.display = 'none';
+  }, 1000);
+
+  setTimeout(() => {
+    card.style.display = 'flex';
+    fadeIn();
+  }, 3000);
+});
