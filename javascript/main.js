9c23efcf2ce0217e9f9a65828914e47b888b00c7
Paul <pdbhomemail@gmail.com>
2023-08-15 01:38:52 +0200

    Updated SSs and commented out incomplete project

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,7 @@
 const projects = [
     { title: 'Challenge 170 - Contact Card', folder: '/iCodeThis-Challenges/Challenge_170-Contact_Card', image: '/iCodeThis-Challenges/Challenge_170-Contact_Card/screenshot.png' },
     { title: 'Challenge 230 - Colored Cards', folder: '/iCodeThis-Challenges/Challenge_230-Colored_Cards', image: '/iCodeThis-Challenges/Challenge_230-Colored_Cards/screenshot.png' },
-    { title: 'Challenge 233 - Profile Card', folder: '/iCodeThis-Challenges/Challenge_233-Profile_Card', image: '/iCodeThis-Challenges/Challenge_233-Profile_Card/screenshot.png' },
+    // Not yet completed { title: 'Challenge 233 - Profile Card', folder: '/iCodeThis-Challenges/Challenge_233-Profile_Card', image: '/iCodeThis-Challenges/Challenge_233-Profile_Card/screenshot.png' },
     { title: 'Challenge 244 - Order Summary', folder: '/iCodeThis-Challenges/Challenge_244-Order_Summary', image: '/iCodeThis-Challenges/Challenge_244-Order_Summary/screenshot.png' },
     { title: 'Challenge 262 - Blog Cards 4', folder: '/iCodeThis-Challenges/Challenge_262-Blog_Cards_4', image: '/iCodeThis-Challenges/Challenge_262-Blog_Cards_4/screenshot.png' },
     { title: 'Challenge 283 - Book Card', folder: '/iCodeThis-Challenges/Challenge_283-Book_Card', image: '/iCodeThis-Challenges/Challenge_283-Book_Card/screenshot.png' },
