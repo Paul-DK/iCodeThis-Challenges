8421f137527a4edd28aa0e0a90ebcd81501b1525
Paul <pdbhomemail@gmail.com>
2023-08-14 23:44:08 +0200

    Altering paths in JS file

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,10 @@
 const projects = [
-    { title: 'Challenge 170 - Contact Card', folder: '/Challenge_170-Contact_Card', image: '/Challenge_170-Contact_Card/screenshot.png' },
-    { title: 'Challenge 230 - Colored Cards', folder: '/Challenge_230-Colored_Cards', image: '/Challenge_230-Colored_Cards/screenshot.png' },
-    { title: 'Challenge 233 - Profile Card', folder: '/Challenge_233-Profile_Card', image: '/Challenge_233-Profile_Card/screenshot.png' },
-    { title: 'Challenge 244 - Order Summary', folder: '/Challenge_244-Order_Summary', image: '/Challenge_244-Order_Summary/screenshot.png' },
-    { title: 'Challenge 262 - Blog Cards 4', folder: '/Challenge_262-Blog_Cards_4', image: '/Challenge_262-Blog_Cards_4/screenshot.png' },
-    { title: 'Challenge 283 - Book Card', folder: '/Challenge_283-Book_Card', image: '/Challenge_283-Book_Card/screenshot.png' },
+    { title: 'Challenge 170 - Contact Card', folder: 'Challenge_170-Contact_Card', image: 'Challenge_170-Contact_Card/screenshot.png' },
+    { title: 'Challenge 230 - Colored Cards', folder: 'Challenge_230-Colored_Cards', image: 'Challenge_230-Colored_Cards/screenshot.png' },
+    { title: 'Challenge 233 - Profile Card', folder: 'Challenge_233-Profile_Card', image: 'Challenge_233-Profile_Card/screenshot.png' },
+    { title: 'Challenge 244 - Order Summary', folder: 'Challenge_244-Order_Summary', image: 'Challenge_244-Order_Summary/screenshot.png' },
+    { title: 'Challenge 262 - Blog Cards 4', folder: 'Challenge_262-Blog_Cards_4', image: 'Challenge_262-Blog_Cards_4/screenshot.png' },
+    { title: 'Challenge 283 - Book Card', folder: 'Challenge_283-Book_Card', image: 'Challenge_283-Book_Card/screenshot.png' },
 ];
 
 const projectsDiv = document.getElementById('projects');
