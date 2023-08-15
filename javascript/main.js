b6f1764f08d7814bc1d3da4e44b42a2fe04ca94e
Paul <pdbhomemail@gmail.com>
2023-08-15 08:55:21 +0200

    Image hover correction

--- javascript/main.js
+++ javascript/main.js
@@ -51,20 +51,20 @@ function createProjectCards() {
   projects.forEach((project) => {
     const card = `
       <div class="col-md-4">
-        <div class="card">
-          <div class="img-and-button">
-            <div class="project-image">
-              <img src="${project.image}" alt="${project.title}">
-              <div class="project-overlay">
-                <p>${project.description}</p>
-                <ul>${project.details.map(detail => `<li>${detail}</li>`).join('')}</ul>
-              </div>
-            </div>
-            <h3>${project.title}</h3>
-            <a href="${project.folder}" class="btn btn-primary">View Project</a>
+    <div class="card">
+      <div class="img-and-button">
+        <div class="project-image">
+          <img src="${project.image}" alt="${project.title}">
+          <div class="project-overlay">
+            <p>${project.description}</p>
+            <ul>${project.details.map(detail => `<li>${detail}</li>`).join('')}</ul>
           </div>
         </div>
+        <h3>${project.title}</h3>
+        <a href="${project.folder}" class="btn btn-primary">View Project</a>
       </div>
+    </div>
+  </div>
     `;
     content += card;
   });
