041fb5633abf95b3fec5822666348fae9db0b2c1
Paul <pdbhomemail@gmail.com>
2023-08-15 02:15:05 +0200

    Adding a details overlay to images on hover

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,48 @@
 const projects = [
-    { title: 'Challenge 170 - Contact Card', folder: '/iCodeThis-Challenges/Challenge_170-Contact_Card', image: '/iCodeThis-Challenges/Challenge_170-Contact_Card/screenshot.png' },
-    { title: 'Challenge 230 - Colored Cards', folder: '/iCodeThis-Challenges/Challenge_230-Colored_Cards', image: '/iCodeThis-Challenges/Challenge_230-Colored_Cards/screenshot.png' },
-    // Not yet completed { title: 'Challenge 233 - Profile Card', folder: '/iCodeThis-Challenges/Challenge_233-Profile_Card', image: '/iCodeThis-Challenges/Challenge_233-Profile_Card/screenshot.png' },
-    { title: 'Challenge 244 - Order Summary', folder: '/iCodeThis-Challenges/Challenge_244-Order_Summary', image: '/iCodeThis-Challenges/Challenge_244-Order_Summary/screenshot.png' },
-    { title: 'Challenge 262 - Blog Cards 4', folder: '/iCodeThis-Challenges/Challenge_262-Blog_Cards_4', image: '/iCodeThis-Challenges/Challenge_262-Blog_Cards_4/screenshot.png' },
-    { title: 'Challenge 283 - Book Card', folder: '/iCodeThis-Challenges/Challenge_283-Book_Card', image: '/iCodeThis-Challenges/Challenge_283-Book_Card/screenshot.png' },
+    { 
+      title: 'Challenge 170 - Contact Card', 
+      folder: '/iCodeThis-Challenges/Challenge_170-Contact_Card', 
+      image: '/iCodeThis-Challenges/Challenge_170-Contact_Card/screenshot.png',
+      description: 'Recreate this design using HTML/CSS. Bonus points for adding:',
+      details: ['Animations', 'JavaScript']
+    },
+    {
+      title: 'Challenge 230 - Colored Cards',
+      folder: '/iCodeThis-Challenges/Challenge_230-Colored_Cards',
+      image: '/iCodeThis-Challenges/Challenge_230-Colored_Cards/screenshot.png',
+      description: 'Recreate this design using HTML/CSS. Bonus points for adding:',
+      details: ['Animations', 'JavaScript']
+    },
+    // Not yet completed
+    //
+    // {
+    //   title: 'Challenge 233 - Profile Card',
+    //   folder: '/iCodeThis-Challenges/Challenge_233-Profile_Card',
+    //   image: '/iCodeThis-Challenges/Challenge_233-Profile_Card/screenshot.png',
+    //   description: 'A short paragraph about the project.',
+    //   details: ['Item 1', 'Item 2']
+    // },
+    {
+      title: 'Challenge 244 - Order Summary',
+      folder: '/iCodeThis-Challenges/Challenge_244-Order_Summary',
+      image: '/iCodeThis-Challenges/Challenge_244-Order_Summary/screenshot.png',
+      description: 'Recreate this design using HTML/CSS. Bonus points for adding:',
+      details: ['Animations', 'JavaScript']
+    },
+    {
+      title: 'Challenge 262 - Blog Cards 4',
+      folder: '/iCodeThis-Challenges/Challenge_262-Blog_Cards_4',
+      image: '/iCodeThis-Challenges/Challenge_262-Blog_Cards_4/screenshot.png',
+      description: 'Recreate this design using HTML/CSS. Bonus points for adding:',
+      details: ['Animations', 'JavaScript']
+    },
+    {
+      title: 'Challenge 283 - Book Card',
+      folder: '/iCodeThis-Challenges/Challenge_283-Book_Card',
+      image: '/iCodeThis-Challenges/Challenge_283-Book_Card/screenshot.png',
+      description: 'Recreate this design using HTML/CSS. Bonus points for adding:',
+      details: ['Animations', 'JavaScript']
+    },
 ];
 
 const projectsDiv = document.getElementById('projects');
@@ -41,6 +79,25 @@ projects.forEach(project => {
     cardBody.appendChild(link);
 
     card.appendChild(cardBody);
+    projectDiv.appendChild(card);
+    projectsDiv.appendChild(projectDiv);
+
+    const overlay = document.createElement('div');
+    overlay.className = 'project-overlay';
+  
+    const description = document.createElement('p');
+    description.innerText = project.description;
+    overlay.appendChild(description);
+  
+    const list = document.createElement('ul');
+    project.details.forEach(detail => {
+      const listItem = document.createElement('li');
+      listItem.innerText = detail;
+      list.appendChild(listItem);
+    });
+    overlay.appendChild(list);
+
+    card.appendChild(overlay);
     projectDiv.appendChild(card);
     projectsDiv.appendChild(projectDiv);
 });
